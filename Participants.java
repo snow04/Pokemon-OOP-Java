fe5accc5ec6bf2a3b72f3022b2f30c6e61c548a0
snow04 <agabriel.nieve@gmail.com>
2020-12-06 22:23:38 +0800

    Add participants for Battle

---
+++ Participants.java
@@ -0,0 +1,9 @@
+class Participants extends Person {
+
+    public static void main(String[] args) {
+        
+        Participants p = new Participants();
+        p.setUsername("Gabriel");
+    }
+    
+}
